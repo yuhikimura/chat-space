eaa9e215a849a0a094cde8ec42c498606897deb8
yuhikimura <yuhi.kimura.0720@gmail.com>
2020-05-31 12:36:33 +0900

    自動更新機能の実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,26 +2,26 @@ $(function(){
   function buildHTML(message){
    if ( message.image ) {
      var html =
-      `<div class="message">
-         <div class="main_chat__messages__message">
-           <div class="main_chat__messages__message__info__talker">
-             ${message.user_name}
-           </div>
-           <div class="main_chat__messages__message__info__date">
-             ${message.created_at}
-           </div>
-         </div>
-         <div class="main_chat__messages__message__text">
-           <p class="main_chat__messages__message__text__content">
-             ${message.content}
-           </p>
-         </div>
-         <img src=${message.image} >
-       </div>`
-     return html;
+        `<div class="message" data-message-id=${message.id}>
+          <div class="main_chat__messages__message">
+            <div class="main_chat__messages__message__info__talker">
+              ${message.user_name}
+            </div>
+            <div class="main_chat__messages__message__info__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="main_chat__messages__message__text">
+            <p class="main_chat__messages__message__text__content">
+              ${message.content}
+            </p>
+          </div>
+          <img src=${message.image} >
+        </div>`
+      return html;
    } else {
      var html =
-      `<div class="message">
+      `<div class="message" data-message-id=${message.id}>
          <div class="main_chat__messages__message">
            <div class="main_chat__messages__message__info__talker">
              ${message.user_name}
@@ -61,5 +61,31 @@ $(function(){
     .fail(function() {
       alert("メッセージ送信に失敗しました");
     });
-})
+  })
+
+  var reloadMessages = function() {
+    var last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.messages').append(insertHTML);
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
